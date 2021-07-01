7d347e7087e1d7a28b2aa82352ce2535121ed3e5
hades1712 <37872657+hades1712@users.noreply.github.com>
2021-07-01 22:25:00 +0800

    Update cdk_trip_stack.py

--- cdk_trip/cdk_trip_stack.py
+++ cdk_trip/cdk_trip_stack.py
@@ -6,7 +6,8 @@ from aws_cdk import core as cdk
 # being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import (
     aws_ec2 as ec2,
-    core    
+    core,
+    aws_s3 as s3
 )
 
 
@@ -16,24 +17,32 @@ class CdkStack(cdk.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         self.vpc = ec2.Vpc(self, "VPC",
-                        cidr="10.0.0.0/16",
+                        cidr="10.0.0.0/17",
                         max_azs=3,
                         subnet_configuration=[ec2.SubnetConfiguration(
                             subnet_type=ec2.SubnetType.PUBLIC,
                             name="Public",
-                            cidr_mask=24
+                            cidr_mask=21
                         ), ec2.SubnetConfiguration(
                             subnet_type=ec2.SubnetType.PRIVATE,
                             name="Private",
-                            cidr_mask=24
+                            cidr_mask=20
                         ), ec2.SubnetConfiguration(
                             subnet_type=ec2.SubnetType.ISOLATED,
                             name="DB",
-                            cidr_mask=24
+                            cidr_mask=22
                         )
                         ],
-                        # nat_gateway_provider=ec2.NatProvider.gateway(),
-                        nat_gateways=3,
+                        nat_gateways=2,
                         )
-        core.CfnOutput(self, "Output",
-                    value=self.vpc.vpc_id)
+        self.vpc.add_s3_endpoint("prodctvpcgateway")             #endpoint
+        bucket=s3.Bucket(self,"IBU-dongshichao-mybucket")    
+    def vpcoutput(self):
+            output= {"vpc_cidr_block":self.vpc.vpc_cidr_block,"vpc_id":self.vpc.vpc_id,
+                        "public_subnets":self.vpc.public_subnets,"private_subnets":self.vpc.private_subnets,
+                        "DBsubnets":self.vpc.isolated_subnets}
+            for k, v in output.items():
+
+                core.CfnOutput(self, k,
+                            value=str(v)
+                            )      
